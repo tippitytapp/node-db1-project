0f9fc16cf0a5a4d83575d0edf7243c3255a34072
tippitytapp <marc.tapp@gmail.com>
2020-05-11 10:40:23 -0400

    updated initial server info to my liking

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const colors = require('colors');
 
 const db = require("../data/dbConfig.js");
 
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+const morgan = require('morgan');
+const helmet = require('helmet');
+const colors = require('colors');
 const server = require("./api/server.js");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+if(process.env.NODE_ENV === 'development'){
+  server.use(morgan('dev'))
+}
 
 server.listen(PORT, () => {
-  console.log(`\n== API running on port ${PORT} ==\n`);
+  console.log(`\n== API running in ${process.env.NODE_ENV}mode on http://localhost:${PORT} ==\n`.cyan.bold.underline);
 });
